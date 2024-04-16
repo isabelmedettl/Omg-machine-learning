bdd3a5472476b9b8777c18792933e468dc92e9bc
Montgomerie <104102099+MontgomerieC@users.noreply.github.com>
2024-04-16 16:57:16 +0200

    17 path

--- Test/environment_omgml.py
+++ Test/environment_omgml.py
@@ -23,12 +23,13 @@ pdi.FAILSAFE = False
 # Isabel path: C:\\Users\\isabe\\Documents\\ML\\Omg-machine-learning\\c++_mojosabel\\build\\debug\\play.exe
 # Skolsabel path: C:\\Users\\mijo1919\\Documents\\Omg-machine-learning\\c++_mojosabel\\build\\debug\\play.exe
 # Monty path: C:\\Python\\GitHub\\Omg-machine-learning\\c++_mojosabel\\build\\debug\\play.exe
-game_path = "C:\\Python\\GitHub\\Omg-machine-learning\\c++_mojosabel\\build\\debug\\play.exe"  # Replace this with your game path
+# Splab-17 path: C:\\Users\\group4\\Documents\\GitHub\\Omg-machine-learning\\c++_mojosabel\\build\\debug\\play.exe
+game_path = "C:\\Users\\group4\\Documents\\GitHub\\Omg-machine-learning\\c++_mojosabel\\build\\debug\\play.exe"  # Replace this with your game path
 
 # Name of game window
 window_title = "Mojosabel"
 
-fps = 300
+fps = 100
 
 register(
     id="Mojosabel-v0",
